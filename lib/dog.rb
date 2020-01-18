6f132fd62489381a2fd81e536f6bae67bb7f3d13
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-18 04:58:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @mood = mood
+    @owner = owner
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
 class Owner
-  # code goes here
+  @@all= []
+  attr_accessor :pets
+  attr_reader :species, :name
+
+  def initialize(name, species = "human")
+    @species = species
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+    @name = name
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.collect do |species, instances|
+      instances.each do |pet|
+        pet.mood = "nervous"
+      end
+      instances.clear
+    end
+  end
+
+  def list_pets
+    num_dogs = @pets[:dogs].size
+    num_cats = @pets[:cats].size
+    return "I have #{num_dogs} dog(s), and #{num_cats} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
 end
